6996239e57e91021a27de133eafbde292f6d1c87
hhao99 <hhao99@gmail.com>
2022-12-12 21:55:29 -0600

    v0.12 todo app with add remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgModule } from '@angular/core';
 import { TodoAppComponent } from './todo/todo-app/todo-app.component';
 import { TodoItemComponent } from './todo/todo-item/todo-item.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { AddItemComponent } from './todo/add-item/add-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoAppComponent,
     TodoListComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
